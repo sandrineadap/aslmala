c3f1f5c8f0938166f875c116da0bdb8bad90d593
sandrine <sandrine@andrews.edu>
2024-04-03 08:53:28 -0400

    fixed title format on recording screen

--- src/screens/RecordingScreen.js
+++ src/screens/RecordingScreen.js
@@ -5,7 +5,11 @@ import Button from '../components/atoms/Button';
 export default function RecordingScreen() {
   return (
     <SafeAreaView style={styles.container}>
+      <View style={{flex: 1}} />
+      <View style={[{ flex: 5, flexDirection: 'column', marginHorizontal: 30, marginBottom: 100 }]}>
         <Text style={[globalStyles.sectionTitle, { flex: 1 }]}>Practice</Text>
+      </View>
+        
     </SafeAreaView>
   );
 }
